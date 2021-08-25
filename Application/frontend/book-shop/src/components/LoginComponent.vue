56a60bb40005538260566c76df16d90aa58f126d
Kwok Chung Kwan <kwokchung.kwan@student.adelaide.edu.au>
2021-08-25 15:45:49 +0930

    Creating SQL query for table and database

--- Application/frontend/book-shop/src/components/LoginComponent.vue
+++ Application/frontend/book-shop/src/components/LoginComponent.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="login_component">
     <h1>{{ msg }}</h1>
-
+    
     <form>
       <div class="mb-3">
         <label for="user_id">User ID:</label><br>
-        <input type="text" class="form-control" id="user_id" name="user_id" value=""><br>
+        <input type="text" class="form-control form-control-sm" id="user_id" name="user_id" value=""><br>
       </div>
 
       <div class="mb-3">
         <label for="password">Last name:</label><br>
-        <input type="text" class="form-control" id="password" name="password" value=""><br><br>
+        <input type="text" class="form-control form-control-sm" id="password" name="password" value=""><br><br>
       </div>
 
       <button type="submit" class="btn btn-primary">Login</button>
--- Application/frontend/book-shop/src/router/index.js
+++ Application/frontend/book-shop/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
@@ -8,8 +7,8 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Login',
+    component: Login
   },
   {
     path: '/login',
