81d03d24f88744a3cdaa231c30c38ab427b979f1
Kwok Chung Kwan <kwokchung.kwan@student.adelaide.edu.au>
2021-08-25 15:04:37 +0930

    Create structure and folder for login page

---
+++ Application/frontend/book-shop/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">
+    <img alt="Vue logo" src="../assets/User_Login_Icon.png">
+    <LoginComponent msg="Welcome to Book Store App"/>
+  </div>
+</template>
+
+<script>
+import LoginComponent from '@/components/LoginComponent.vue'
+
+export default {
+  name: 'Login',
+  components: {
+    LoginComponent
+  }
+}
+</script>
