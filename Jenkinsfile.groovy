58f2554d8c85652c9dfdc83cfbe460e9c86e74f0
Yifei Xu <appmocca@gmail.com>
2021-10-31 12:42:46 +0800

    update

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,17 +1,17 @@
 #!/usr/bin/env groovy
 // 镜像仓库地址需要时打开
 // 使用需要下载pipline
-def registry = "xxx"
-def project = "max"
-def app_name = "java-demo"
-def image_name = "springboot-node"
+// def registry = "xxx"
+// def project = "max"
+// def app_name = "java-demo"
+// def image_name = "springboot-node"
 def git_address = "https://github.com/appmocca/CONT8.git"
 pipeline {
     agent any
     stages {
         stage('Pulling code from github'){
             steps {
-             sh 'git clone https://github.com/appmocca/CONT8.git'
+             sh "git clone ${git_address}"
             }
         }
 
@@ -26,7 +26,7 @@ pipeline {
                 sh 'npm --version'
                 }
            } 
-        }
+        
 
         stage('Docker implement'){
            steps {
@@ -34,3 +34,4 @@ pipeline {
             }
         }
     }
+}
