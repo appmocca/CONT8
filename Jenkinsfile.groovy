44706d427d9ec5c5ab15720aaaa085fa622cc6a9
Yifei Xu <appmocca@gmail.com>
2021-11-02 17:22:04 +0800

    update

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,7 +11,7 @@ pipeline {
     stages {
         stage('拉取代码'){
             steps {
-             sh 'git pull https://github.com/appmocca/CONT8.git -b max'
+             sh 'git pull https://github.com/appmocca/CONT8.git'
             // 拉取代码、
             }
         }
